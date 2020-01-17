962b506e253086b23d6080e8c1d95fdfeb299efb
“Tuszyk <info@tuszyk.com>
2020-01-17 17:32:44 +0100

    create app structure

--- megashop/src/app/app.module.ts
+++ megashop/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { ArticleComponent } from './views/article/article.component';
+import { BasketComponent } from './views/basket/basket.component';
+import { ComparisonComponent } from './views/comparison/comparison.component';
+import { HomeComponent } from './views/home/home.component';
+import { ListComponent } from './views/list/list.component';
+import { ProductComponent } from './views/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    ProductCardComponent,
+    SideMenuComponent,
+    ArticleComponent,
+    BasketComponent,
+    ComparisonComponent,
+    HomeComponent,
+    ListComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
---
+++ megashop/src/app/views/comparison/comparison.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ComparisonRoutingModule } from './comparison-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ComparisonRoutingModule
+  ]
+})
+export class ComparisonModule { }
