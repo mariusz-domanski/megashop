358875c8291916a4d686af20dea7c8566a760f7e
Mariusz Domanski <mariusz@domanski.pl>
2020-01-22 00:03:35 +0100

    Create header

--- megashop/src/app/services/api.service.ts
+++ megashop/src/app/services/api.service.ts
@@ -5,5 +5,24 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
+  public menu: any = [
+    {
+      'Id' : 1,
+      'Name' : 'Startowa',
+    },
+    {
+      'Id' : 2,
+      'Name' : 'Menu 1'
+    },
+    {
+      'Id' : 3,
+      'Name' : 'Menu 2'
+    },
+    {
+      'Id': 4,
+      'Name' : 'Menu 3'
+    }
+  ];
+
   constructor() { }
 }
