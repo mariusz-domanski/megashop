62ebea6788efa1096110f9f26b0b67d6941d63b8
Mariusz Domanski <mariusz@domanski.pl>
2020-01-22 23:07:38 +0100

    Routing header component

--- megashop/src/app/app-routing.module.ts
+++ megashop/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './views/list/list.component';
+import { HomeComponent } from './views/home/home.component';
+import { ArticleComponent } from './views/article/article.component';
+import { BasketComponent } from './views/basket/basket.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'soki-owocowe', component: ListComponent},
+  {path: 'soki-warzywne', component: ListComponent},
+  {path: 'soki-mieszane', component: ListComponent},
+  {path: 'o-nas', component: ArticleComponent},
+  {path: 'polityka-prywatnosci', component: ArticleComponent},
+  {path: 'cennik-dostaw', component: ArticleComponent},
+  {path: 'kontakt', component: ArticleComponent},
+  {path: 'koszyk', component: BasketComponent},
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+  {path: '**', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- megashop/src/app/services/api.service.ts
+++ megashop/src/app/services/api.service.ts
@@ -6,14 +6,14 @@ import { Injectable } from '@angular/core';
 export class ApiService {
 
   public menu: any = [
-    { Id : 1, Name : 'Strona główna' },
-    { Id : 2, Name : 'Soki owocowe' },
-    { Id : 3, Name : 'Soki warzywne'},
-    { Id : 4, Name : 'Soki mieszane'},
-    { id : 5, Name : 'O nas'},
-    { id : 6, Name : 'Polityka prywatności'},
-    { id : 7, Name : 'Cennik dostaw'},
-    { id : 8, Name : 'Kontakt'}
+    { Id : 1, Name : 'Strona główna', Link: '/' },
+    { Id : 2, Name : 'Soki owocowe', Link: 'soki-owocowe' },
+    { Id : 3, Name : 'Soki warzywne', Link: 'soki-warzywne'},
+    { Id : 4, Name : 'Soki mieszane', Link: 'soki-mieszane'},
+    { id : 5, Name : 'O nas', Link: 'o-nas'},
+    { id : 6, Name : 'Polityka prywatności', Link: 'polityka-prywatnosci'},
+    { id : 7, Name : 'Cennik dostaw', Link: 'cennik-dostaw'},
+    { id : 8, Name : 'Kontakt', Link: 'kontakt'}
   ];
 
   constructor() { }
